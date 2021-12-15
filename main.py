d047a351d07c648af4e93b12dec412a843add0d5
ROana-coder <59318326+ROana-coder@users.noreply.github.com>
2021-12-15 17:19:42 +0200

    1st

---
+++ main.py
@@ -0,0 +1,9 @@
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+
+train_data=pd.read_csv('train.csv.zip')
+#print(train_data)
+#print(train_data.shape)
+#print(train_data.info())
